4413f7de67ba98e6aecab3dc748665cd1e0bd0fb
Nico Hahn <6137959-nicohahn@users.noreply.gitlab.com>
2021-11-08 15:56:50 +0100

    fix migration datatype

--- db/migrate/20211108144910_create_projects.rb
+++ db/migrate/20211108144910_create_projects.rb
@@ -1,7 +1,7 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
-      t.int :external_id
+      t.integer :external_id
       t.string :external_key
 
       t.timestamps
