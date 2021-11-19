0e7e68dfa3cda25fd058ed5c43109639fe6fe32b
Nico Hahn <6137959-nicohahn@users.noreply.gitlab.com>
2021-11-19 20:41:46 +0100

    cleanup, :de locale, issue_templates templates

--- app/controllers/issue_templates_controller.rb
+++ app/controllers/issue_templates_controller.rb
@@ -1,4 +1,7 @@
 class IssueTemplatesController < ApplicationController
+
+  before_action :set_issue_template, except: [:new, :create, :index]
+
   def index
     @templates = IssueTemplate.all
   end
@@ -11,9 +14,13 @@ class IssueTemplatesController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    if @issue_template.update(params.require(:issue_template).permit(:title))
+      render json: {success: true}
+    end
   end
 
   def destroy
@@ -21,10 +28,16 @@ class IssueTemplatesController < ApplicationController
 
   def add_attribute
     if params[:id].blank?
-      issue_template = IssueTemplate.create(title: params[:title])
-      IssueTemplateAttribute.create(issue_template_id: issue_template.id, attribute_type: params[:attribute_id].to_i)
-      render json: {location: "/issue_templates/#{issue_template.id}/edit"}
+      @issue_template = IssueTemplate.create(title: params[:title])
     end
+    IssueTemplateAttribute.create(issue_template_id: issue_template.id, attribute_type: params[:attribute_id].to_i)
+    render json: {location: "/issue_templates/#{issue_template.id}/edit"}
+  end
+
+  private
+
+  def set_issue_template
+    @issue_template = IssueTemplate.find_by!(id: params[:id])
   end
 
 end
--- app/javascript/packs/issue_templates.js
+++ app/javascript/packs/issue_templates.js
@@ -1,20 +1,33 @@
 $(document).on('click', '.add_attribute', function(e){
-    console.log($(this).data('id'));
-    console.log($('#issue_template_issue_template_attributes').val());
-
-
     let selected = $('#issue_template_issue_template_attributes').val(),
         id = $(this).data('id'),
         title = $('#issue_template_title').val();
 
-    $.ajax('/issue_templates/add_attribute', {
-        method: 'POST',
-        data: {id: id, attribute_id: selected, title: title}
+    if (selected==="") {
+        alert('Bitte ein Feld wählen!')
+        e.preventDefault();
+    } else {
+        $.ajax('/issue_templates/add_attribute', {
+            method: 'POST',
+            data: {id: id, attribute_id: selected, title: title}
+        }).done(function(data){
+            location.href = data.location;
+        })
+        e.preventDefault();
+    }
+});
+
+$(document).on('click', 'button[type=submit]', function(e){
+    $(this).children('#loader').show();
+    $(this).children('#prev_loader').hide();
+    e.preventDefault();
+    $.ajax($(this).closest('form').data("url"), {
+        method: 'PATCH',
+        data: $(this).closest('form').serialize()
     }).done(function(data){
-        location.href = data.location;
+        $('#loader').hide();
+        $('#prev_loader').show();
     })
     e.preventDefault();
 });
-
-
 
